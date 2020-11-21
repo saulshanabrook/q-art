ac4403d20f6ba3be15ca34056a3369b7780adcf7
Saul Shanabrook <s.shanabrook@gmail.com>
2020-11-21 10:40:15 -0500

    Some weird scrolling things

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-console.log("hello world");
+const nImages = 2;
+const scrollPxPerImage = 1000;
 
-function hi() {
-  const i = 1;
-  console.log("hi!");
-  return i;
+document.documentElement.style.height = `calc(${
+  nImages * scrollPxPerImage
+}px + 100vh)`;
+
+function setScale(image, scale) {
+  document.querySelector(
+    `#images > img:nth-child(${image + 1})`
+  ).style.transform = `translateX(-50%) scale(${scale})`;
+}
+function onScroll() {
+  // The "scrollPosition" is a number from 0 to the number of images.
+  const scrollPosition = window.scrollY / scrollPxPerImage;
+
+  const backgroundImage = Math.floor(scrollPosition);
+  const foregroundImage = Math.ceil(scrollPosition);
+
+  const percentMoved = scrollPosition - backgroundImage;
+  console.log({ backgroundImage, foregroundImage, percentMoved });
+
+  setScale(backgroundImage, 1 + percentMoved);
+  setScale(foregroundImage, percentMoved);
+  // root.style.setProperty('--mouse-x', e.clientX + "px");
 }
 
-hi();
+window.addEventListener("scroll", onScroll);
