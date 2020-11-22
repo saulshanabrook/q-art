f1eb351b83bdeebb882747a816dccfa24fdcae87
Saul Shanabrook <s.shanabrook@gmail.com>
2020-11-22 15:52:13 -0500

    Try switching to just zoom in

--- main.js
+++ main.js
@@ -6,35 +6,38 @@ document.documentElement.style.height = `calc(${
 }px + 100vh)`;
 
 function setStyle(image, scale, opacity) {
-  const style = document.querySelector(
-    `#images > img:nth-child(${image + 1})`
-  ).style
+  const style = document.querySelector(`#images > img:nth-child(${image + 1})`)
+    .style;
+  // at the end, it should be scaled to the number
   style.transform = `translateX(-50%) scale(${scale})`;
   style.opacity = opacity;
 }
 
 function onScroll() {
   // The "scrollPosition" is a number from 0 to the number of images.
-  const scrollPosition = window.scrollY / scrollPxPerImage;
+  const scrollPercent = window.scrollY / (nImages * scrollPxPerImage);
+  document.querySelector(`#images`).style.transform = `scale(${Math.pow(
+    100,
+    scrollPercent * nImages
+  )})`;
+  // const backgroundImage = Math.floor(scrollPosition);
+  // const foregroundImage = Math.ceil(scrollPosition);
 
-  const backgroundImage = Math.floor(scrollPosition);
-  const foregroundImage = Math.ceil(scrollPosition);
+  // const percentMoved = scrollPosition - backgroundImage;
 
-  const percentMoved = scrollPosition - backgroundImage;
-
-  // Set background image to be scaled up as we move and so with foreground
-  for (let i = 0; i <= nImages; i++) {
-    switch (i) {
-      case backgroundImage:
-        setStyle(i, 1 + percentMoved, 1 - percentMoved);
-        break;
-      case foregroundImage:
-        setStyle(i, percentMoved, 1);
-        break;
-      default:
-        setStyle(i, 0, 0);
-    }
-  }
+  // // Set background image to be scaled up as we move and so with foreground
+  // for (let i = 0; i <= nImages; i++) {
+  //   switch (i) {
+  //     case backgroundImage:
+  //       setStyle(i, 1 + percentMoved, 1 - percentMoved);
+  //       break;
+  //     case foregroundImage:
+  //       setStyle(i, percentMoved, 1);
+  //       break;
+  //     default:
+  //       setStyle(i, 0, 0);
+  //   }
+  // }
 }
 
 window.addEventListener("scroll", onScroll);
